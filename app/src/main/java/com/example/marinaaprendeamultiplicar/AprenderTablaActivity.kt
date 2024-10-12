7de822843d685ea23970ef697b20ef40cdb056aa
hector <hilariocamachomore@gmail.com>
2024-10-12 21:49:34 +0200

    initial commit

--- app/src/main/java/com/example/marinaaprendeamultiplicar/AprenderTablaActivity.kt
+++ app/src/main/java/com/example/marinaaprendeamultiplicar/AprenderTablaActivity.kt
@@ -1,34 +1,71 @@
 package com.example.marinaaprendeamultiplicar
 
-
-import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import android.text.style.UnderlineSpan
 
 class AprenderTablaActivity : AppCompatActivity() {
 
-    @SuppressLint("MissingInflatedId")
+    companion object {
+        const val NUMERO_TABLA = "NUMERO_TABLA"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aprender_tabla)
 
-        val tablaLayout = findViewById<LinearLayout>(R.id.tablaLayout)
-        val numeroTabla = intent.getIntExtra("NUMERO_TABLA", 1)
-        val colors = listOf(
-            R.color.blue_1, R.color.blue_2, R.color.blue_3, R.color.blue_4,
-            R.color.blue_5, R.color.blue_6, R.color.blue_7, R.color.blue_8,
-            R.color.blue_9, R.color.blue_10
-        )
+        val numeroTabla = intent.getIntExtra(NUMERO_TABLA, 1)
 
+        val tvTablaTitulo = findViewById<TextView>(R.id.tvTablaTitulo)
+        val tvNumeroTabla = findViewById<TextView>(R.id.tvNumeroTabla)
+
+        val tituloSpannable = SpannableString(tvTablaTitulo.text)
+        tituloSpannable.setSpan(UnderlineSpan(), 0, tituloSpannable.length, 0)
+        tvTablaTitulo.text = tituloSpannable
+
+        val numeroSpannable = SpannableString(tvNumeroTabla.text)
+        numeroSpannable.setSpan(UnderlineSpan(), 0, numeroSpannable.length, 0)
+        tvNumeroTabla.text = numeroSpannable
+
+        val tvTablaContenido = findViewById<TextView>(R.id.tvTablaContenido)
+        tvTablaContenido.text = generarTablaMultiplicarConColores(numeroTabla)
+    }
+
+    private fun generarTablaMultiplicarConColores(numero: Int): SpannableString {
+        val sb = StringBuilder()
         for (i in 1..10) {
-            val textView = TextView(this)
-            textView.text = "$numeroTabla x $i = ${numeroTabla * i}"
-            textView.textSize = 20f
-            textView.setTextColor(ContextCompat.getColor(this, colors[i - 1]))
-            tablaLayout.addView(textView)
+            sb.append("$numero x $i = ${numero * i}\n")
         }
+
+        val spannableString = SpannableString(sb.toString())
+        val lines = sb.toString().split("\n")
+
+        var start = 0
+        for (i in lines.indices) {
+            val end = start + lines[i].length
+            val color = when (i + 1) {
+                1 -> ContextCompat.getColor(this, R.color.blue_1)
+                2 -> ContextCompat.getColor(this, R.color.blue_2)
+                3 -> ContextCompat.getColor(this, R.color.blue_3)
+                4 -> ContextCompat.getColor(this, R.color.blue_4)
+                5 -> ContextCompat.getColor(this, R.color.blue_5)
+                6 -> ContextCompat.getColor(this, R.color.blue_6)
+                7 -> ContextCompat.getColor(this, R.color.blue_7)
+                8 -> ContextCompat.getColor(this, R.color.blue_8)
+                9 -> ContextCompat.getColor(this, R.color.blue_9)
+                10 -> ContextCompat.getColor(this, R.color.blue_10)
+                else -> Color.BLACK // Color por defecto si hay más de 10 líneas
+            }
+            spannableString.setSpan(ForegroundColorSpan(color), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            start = end + 1 // +1 para incluir el salto de línea
+        }
+
+        return spannableString
     }
 }
