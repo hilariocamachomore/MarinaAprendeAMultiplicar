85dbd0ba613cbc7220bc3eed139119ab1d9206ab
hector <hilariocamachomore@gmail.com>
2024-10-20 21:57:04 +0200

    commit desde casa

--- app/src/main/java/com/example/marinaaprendeamultiplicar/AprenderTablaActivity.kt
+++ app/src/main/java/com/example/marinaaprendeamultiplicar/AprenderTablaActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 
 class AprenderTablaActivity : AppCompatActivity() {
 
@@ -12,12 +13,23 @@ class AprenderTablaActivity : AppCompatActivity() {
         const val EXTRA_NUMERO_TABLA = "NUMERO_TABLA"
     }
 
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aprender_tabla)
 
         val numeroTabla = intent.getIntExtra(EXTRA_NUMERO_TABLA, 1)
         generateTabla(numeroTabla)
+
+        mediaPlayer = when (numeroTabla) {
+            in 1..5 -> MediaPlayer.create(this, R.raw.tablaunomarina)
+            in 6..10 -> MediaPlayer.create(this, R.raw.ca)
+            else -> null
+        }
+
+        mediaPlayer?.isLooping = true
+        mediaPlayer?.start()
     }
 
     private fun generateTabla(numeroTabla: Int) {
@@ -25,10 +37,20 @@ class AprenderTablaActivity : AppCompatActivity() {
         // Get the TextView from the layout
         val tablaTextView: TextView = findViewById(R.id.tablaTextView)
 
+        val multiplicationLine = "TABLA DEL $numeroTabla\n\n"
+        tablaTextView.append(multiplicationLine)
+
         for (i in 1..10) {
             // Append each multiplication line to the TextView
             val multiplicationLine = "$numeroTabla x $i = ${numeroTabla * i}\n"
             tablaTextView.append(multiplicationLine)
         }
     }
+    override fun onStop() {
+        super.onStop()
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
 }
