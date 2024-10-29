02413fefce7cd3de24d7a83771086b4d98794f14
hector <hilariocamachomore@gmail.com>
2024-10-29 18:06:08 +0100

    commit desde casa

--- app/src/main/java/com/example/marinaaprendeamultiplicar/ElegirTablaPracticarActivity.kt
+++ app/src/main/java/com/example/marinaaprendeamultiplicar/ElegirTablaPracticarActivity.kt
@@ -1,30 +1,40 @@
+
+
 package com.example.marinaaprendeamultiplicar
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 
 class ElegirTablaPracticarActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_elegir_tabla_practicar)
 
-        val edtNumeroTabla = findViewById<EditText>(R.id.edtNumeroTabla)
-        val btnContinuar = findViewById<Button>(R.id.btnContinuar)
+        // Configurar listeners para los botones
+        val buttons = arrayOf(
+            findViewById<Button>(R.id.button1),
+            findViewById<Button>(R.id.button2),
+            findViewById<Button>(R.id.button3),
+            findViewById<Button>(R.id.button4),
+            findViewById<Button>(R.id.button5),
+            findViewById<Button>(R.id.button6),
+            findViewById<Button>(R.id.button7),
+            findViewById<Button>(R.id.button8),
+            findViewById<Button>(R.id.button9),
+            findViewById<Button>(R.id.button10)
+        )
 
-        btnContinuar.setOnClickListener {
-            val numeroTablaString = edtNumeroTabla.text.toString()
-            if (numeroTablaString.isNotEmpty()) {
-                val numeroTabla = numeroTablaString.toInt()
-                // Inicia la siguiente Activity y pasa el número de la tabla
+        for (i in buttons.indices) {
+            buttons[i].setOnClickListener {
+                val tablaSeleccionada = i + 1 // +1 porque los índices van de 0 a 9
                 val intent = Intent(this, PracticarTablaActivity::class.java)
-                intent.putExtra("NUMERO_TABLA", numeroTabla)
+                intent.putExtra("NUMERO_TABLA", tablaSeleccionada)
                 startActivity(intent)
-            } else {
-                Toast.makeText(this, "Por favor, escribe un número", Toast.LENGTH_SHORT).show()
             }
         }
     }
